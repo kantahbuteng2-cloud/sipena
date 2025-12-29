e76e4f859e7a5685946f38db001061caea323207
kantahbuteng2-cloud <kantahbuteng2@gmail.com>
2025-12-29 19:04:10 +0800

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const iframe = document.getElementById("gasFrame");
-    iframe.src = "https://script.google.com/macros/s/AKfycbzA2g-Itkr2V1eCsTuLVBYr0114FuKyhQ16DtcvOS7Bwekh7bf9jYDc6IvmcysMy4St5Q/exec";
-});
+// Pastikan iframe selalu menyesuaikan tinggi layar
+window.addEventListener("resize", setIframeHeight);
+window.addEventListener("load", setIframeHeight);
+
+
+function setIframeHeight() {
+const iframe = document.getElementById("sipenaFrame");
+iframe.style.height = window.innerHeight + "px";
+}
